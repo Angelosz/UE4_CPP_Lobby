449b794c610aea188e39a676dc825ac2acb5410c
Angel Cabrera Gil <angelcabgil@gmail.com>
2016-03-15 00:06:09 +0000

    Adds Map and NumberOfMaxConnections

--- SessionSearchResultStruct.h
+++ SessionSearchResultStruct.h
@@ -19,16 +19,28 @@ struct CPP_LOBBYSESSIONS_API FSessionSearchResultStruct
 		int32 NumberOfOpenConnections;
 
 	UPROPERTY(BlueprintReadOnly, Category = "Session Search Result")
+		int32 NumberOfMaxConnections;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Session Search Result")
 		bool bHasPassword;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Session Search Result")
+		FString MapName;
 
 	FSessionSearchResultStruct()
 	{
 		OwningUserId = "User Id";
 		PartyName = "Party Name";
 		NumberOfOpenConnections = 0;
+		NumberOfMaxConnections = 2;
 		bHasPassword = false;
+		MapName = "TestMap1";
 	}
 
-	FSessionSearchResultStruct(FString _OwningUserId, FString _PartyName, int32 _NumberOfOpenConnections, bool _bHasPassword)
-		: OwningUserId(_OwningUserId), PartyName(_PartyName), NumberOfOpenConnections(_NumberOfOpenConnections), bHasPassword(_bHasPassword) {}
+	FSessionSearchResultStruct(FString _OwningUserId, FString _PartyName,
+							   int32 _NumberOfOpenConnections, int32 _NumberOfMaxConnections,
+							   bool _bHasPassword, FString _MapName)
+		: OwningUserId(_OwningUserId), PartyName(_PartyName),
+		NumberOfOpenConnections(_NumberOfOpenConnections), NumberOfMaxConnections(_NumberOfMaxConnections),
+		bHasPassword(_bHasPassword), MapName(_MapName) {}
 };
